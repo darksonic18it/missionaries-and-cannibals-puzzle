02395ec916da194b86e6ce6b88efef1af00b45f0
darksonic18it <khimjayevedientes@gmail.com>
2025-11-01 23:06:57 -0700

    Remove main function from tempCodeRunnerFile.cpp

--- dsa-presentation2/tempCodeRunnerFile.cpp
+++ dsa-presentation2/tempCodeRunnerFile.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include "Solver.h"
-using namespace std;
 
-int main() {
-    cout << "The Missionaries and Cannibals Problem" << endl;
-    cout << endl;
-    cout << "Expected output:" << endl;
-    cout << endl;
-    
-    Solver::solveBFS();
-    
-    return 0;
-}
